2beaa96e276953879752711583334d77a33f1186
Trung N Vong <trungnvong956@g.ucla.edu>
2022-08-04 16:45:02 -0700

    Completed Snack Facts

--- src/screens/SnackFacts.js
+++ src/screens/SnackFacts.js
@@ -1,17 +1,66 @@
 import React from "react";
-import { Box, View, Text } from "native-base";
-
+import {
+  Center,
+  Heading,
+  Box,
+  View,
+  Text,
+  Image,
+  HStack,
+  VStack,
+} from "native-base";
+import OnionIMG from "../../assets/img/img_60.png";
+import Icon from "../../assets/img/img_61.png";
 export default function SnackFacts() {
   return (
-    <View>
-      <Box>Image</Box>
-      <Text>Red Onion</Text>
-      <Box>
-        <Text>
-          Red onions contain at least 25 different anthocyanins. Red onions are
-          also more effective natural blood thinner than white onions
+    <View bg={"white"} height={"100%"} p={5}>
+      <Center>
+        <Heading>Snack Facts</Heading>
+        <Image source={OnionIMG} alt="img" size="2xl" mt={5} />
+        <Text
+          mt={3}
+          bold
+          fontFamily={"Graphik-Medium"}
+          fontSize="3xl"
+          fontStyle={"upperCase"}
+        >
+          RED ONION
         </Text>
-      </Box>
+        <Text
+          bold
+          fontFamily={"Graphik-Medium"}
+          fontSize="xl"
+          fontStyle={"upperCase"}
+        >
+          45 calories
+        </Text>
+        <Box>
+          <HStack justifyContent={"center"} alignItems="center">
+            <Image source={Icon} alt="image" size={"5"} mr={2} />
+            <Text
+              fontFamily={"Graphik-Light"}
+              fontSize={18}
+              fontStyle={"upperCase"}
+            >
+              Red onions are more effective natural blood thinners than white
+              onions
+            </Text>
+          </HStack>
+        </Box>
+
+        <Box mt={5}>
+          <HStack justifyContent={"center"} alignItems="center">
+            <Image source={Icon} alt="image" size={"5"} mr={2} />
+            <Text
+              fontFamily={"Graphik-Light"}
+              fontSize={18}
+              fontStyle={"upperCase"}
+            >
+              Red onions contain at least 25 different anthocyanins.
+            </Text>
+          </HStack>
+        </Box>
+      </Center>
     </View>
   );
 }
